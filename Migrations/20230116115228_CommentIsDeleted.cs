22147513f3e73ebacc8321039325f181def4dea5
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-02-16 11:40:33 +0100

    New migrations with updated models

---
+++ Migrations/20230116115228_CommentIsDeleted.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoCom_API.Migrations
+{
+    public partial class CommentIsDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                schema: "NoCom",
+                table: "comments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                schema: "NoCom",
+                table: "comments");
+        }
+    }
+}
